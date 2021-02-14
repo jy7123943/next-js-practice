387d575107dcf320271bff21bff25f4543ec1154
juyeon <juyeonh@rainist.com>
2021-02-14 20:57:04 +0900

    Add pages

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>테스트</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,9 @@
+const Ninjas = (params) => {
+  return (
+    <div>
+      <h1>Ninjas</h1>
+    </div>
+  );
+}
+
+export default Ninjas;
