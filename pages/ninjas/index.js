55426a2ef22453e5dc968a5467bc9064f069eff3
juyeon <juyeonh@rainist.com>
2021-02-14 21:48:55 +0900

    Add data fetch

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,16 @@
 import Head from 'next/head'
 import styles from '../../styles/Ninjas.module.css'
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
       <Head>
@@ -9,6 +18,13 @@ const Ninjas = () => {
       </Head>
       <div>
         <h1>Ninjas</h1>
+        {ninjas.map(ninja => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   )
