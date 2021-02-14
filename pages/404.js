cf603d1fe74895b62f1c936234083e68e0284b1d
juyeon <juyeonh@rainist.com>
2021-02-14 21:17:34 +0900

    Redirect homepage using useRouter hook

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,18 @@
-import Link from "next/link"
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import {useEffect} from 'react'
 
 const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(-1)
+
+      router.push('/')
+    }, 3000)
+  }, [])
+
   return (
     <div className='not-found'>
       <h1>Oooops...</h1>
