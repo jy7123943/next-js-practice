ee7bc5117f1b3822adf88a1d12b29b99af31eb6d
juyeon <juyeonh@rainist.com>
2021-02-21 22:05:28 +0900

    Get static paths of each detail page

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,19 @@
 import Head from 'next/head'
 
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  const paths = data.map(ninja => ({
+    params: { id: ninja.id.toString() }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
 const Details = () => {
   return (
     <>
